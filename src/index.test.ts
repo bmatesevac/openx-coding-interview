a8525c248b68e057908755f2bdf8fdae0741bd9b
Bob Matesevac <bob@matesevac.com>
2026-01-04 13:28:46 -0500

    remove unneeded code

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,5 @@
-import * as sut from './index';
+//import * as sut from './index';
 
-describe('testFunction', () => {
-  it('should double the input number', () => {
-    expect(sut.testFunction(2)).toBe(4);
-  });
-
+describe('tests', () => {
+  it.todo('placeholder')
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-// Startup file for writing and debugging TypeScript code
-
-
-export function testFunction(input: number): number {
-  return input * 2;
-}
-
 
 function main(): void {
   console.log("Hello OpenX!");
